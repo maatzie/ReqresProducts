8717b3652477b605eb62026f5eee60f63c9a0fac
maatzie <veronikazdanovich@mail.ru>
2023-01-13 17:51:07 +0100

    Products component view created

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,11 +4,14 @@ import SearchBar from './SearchBar';
 import Container from '@mui/material/Container';
 
 class Main extends React.Component {
+    products = [{"id":1,"name":"cerulean","year":2000,"color":"#98B2D1","pantone_value":"15-4020"},{"id":2,"name":"fuchsia rose","year":2001,"color":"#C74375","pantone_value":"17-2031"},{"id":3,"name":"true red","year":2002,"color":"#BF1932","pantone_value":"19-1664"},{"id":4,"name":"aqua sky","year":2003,"color":"#7BC4C4","pantone_value":"14-4811"},{"id":5,"name":"tigerlily","year":2004,"color":"#E2583E","pantone_value":"17-1456"}];
+
+    
     render() {
         return (
             <Container>
                 <SearchBar/>
-                <Products/>
+                <Products products={this.products}/>
             </Container>
         );
     }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-class Products extends React.Component {
+interface Product {
+    id: number,
+    name: string,
+    year: number,
+    color: string,
+}
+
+interface ProductsProps {
+    products: Array<Product>,
+};
+
+class Products extends React.Component<ProductsProps> {
+        
     render() {
         return (
-            <div>
-                
-            </div>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell align="right">Name</TableCell>
+                        <TableCell align="right">Year</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {this.props.products.map((row) => (
+                        <TableRow
+                        key={row.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 }, "background-color": row.color }}
+                        >
+                        <TableCell component="th" scope="row">
+                            {row.id}
+                        </TableCell>
+                        <TableCell align="right">{row.name}</TableCell>
+                        <TableCell align="right">{row.year}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         );
     }
 }
