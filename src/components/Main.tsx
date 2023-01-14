1b292e462a7b11beb2f2621b0804fe4b4c868c60
maatzie <veronikazdanovich@mail.ru>
2023-01-14 18:32:08 +0100

    Pagination added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import Products from './Products';
 import SearchBar from './SearchBar';
 import Container from '@mui/material/Container';
+import Pagination from '@mui/material/Pagination';
+import Box from '@mui/material/Box';
 
 class Main extends React.Component {
     
     state = {
         products: [],
         loading: true,
+        page: 1,
+        pagesNumber: 1,
     }
     
     getProducts = (page: number) => {
         fetch(`https://reqres.in/api/products?page=${page}&per_page=5`)
         .then((data) => data.json())
-            .then((data) => this.setState({products: data.data, loading: false}))
+            .then((data) => this.setState({products: data.data, loading: false, pagesNumber: data.total_pages}))
             .catch((error) => {
                 console.error(error);
                 this.setState({loading: false});
@@ -24,11 +28,16 @@ class Main extends React.Component {
         this.getProducts(2);
     }
 
+    componentDidUpdate(): void {
+        this.getProducts(this.state.page);
+    }
+
     render() {
         return (
             <Container>
-                <SearchBar/>
+                <SearchBar />
                 <Products products={this.state.products}/>
+                <Pagination sx={{width: 'fit-content', margin: 'auto'}} count={this.state.pagesNumber} page={this.state.page} onChange={(event: React.ChangeEvent<unknown>,value:number) => {this.setState({page: value})}} />
             </Container>
         );
     }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 interface Product {
     id: number,
@@ -22,31 +23,33 @@ class Products extends React.Component<ProductsProps> {
         
     render() {
         return (
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell align="right">Name</TableCell>
-                        <TableCell align="right">Year</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {this.props.products.map((row) => (
-                        <TableRow
-                        key={row.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 }, "background-color": row.color }}
-                        >
-                        <TableCell component="th" scope="row">
-                            {row.id}
-                        </TableCell>
-                        <TableCell align="right">{row.name}</TableCell>
-                        <TableCell align="right">{row.year}</TableCell>
+            <Box sx={{minHeight: 350}}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Year</TableCell>
                         </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                        {this.props.products.map((row) => (
+                            <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 }, "backgroundColor": row.color }}
+                            >
+                            <TableCell component="th" scope="row">
+                                {row.id}
+                            </TableCell>
+                            <TableCell align="right">{row.name}</TableCell>
+                            <TableCell align="right">{row.year}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         );
     }
 }
