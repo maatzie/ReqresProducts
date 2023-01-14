14ff3a291f63282101a9bbc0668827a0e1e9f8fd
maatzie <veronikazdanovich@mail.ru>
2023-01-14 19:15:24 +0100

    Roboto font added

---
+++ src/components/NothingFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+function NothingFound() {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default NothingFound;
