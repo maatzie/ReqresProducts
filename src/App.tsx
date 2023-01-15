ea55e05c40d240f71125047a4c4b2e4f0b22152e
maatzie <veronikazdanovich@mail.ru>
2023-01-15 18:15:11 +0100

    URL page parameter changes with pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link, BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Main, {} from './components/Main';
+import Main from './components/Main';
 
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      <BrowserRouter>
+          <Routes>
+              <Route path="*" element={<Main />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,60 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Link, MemoryRouter, Route, Routes, useLocation, useParams } from 'react-router-dom';
 import Products from './Products';
 import SearchBar from './SearchBar';
 import Preloader from './Preloader';
 import Container from '@mui/material/Container';
 import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
 import Box from '@mui/material/Box';
-import CircularProgress from '@mui/material/CircularProgress';
 import NothingFound from './NothingFound';
 
-class Main extends React.Component {
-    
-    state = {
-        products: [],
-        loading: true,
-        page: 1,
-        pagesNumber: 1,
-    }
-    
-    getProducts = (page: number) => {
+function Main() {
+    const [products, setProducts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [pagesNumber, setPagesNumber] = useState(1);
+    const [loading, setLoading] = useState(true);
+
+    const getProducts = (page: number) => {
         fetch(`https://reqres.in/api/products?page=${page}&per_page=5`)
         .then((data) => data.json())
-            .then((data) => this.setState({products: data.data, loading: false, pagesNumber: data.total_pages}))
+            .then((data) => {setProducts(data.data); setLoading(false); setPagesNumber(data.total_pages);})
             .catch((error) => {
                 console.error(error);
-                this.setState({loading: false});
+                setLoading(false);
             });
+    };
+
+    useEffect(() => {
+        getProducts(page);
+    }, [page]);
+
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const currentPage = parseInt(query.get('page') || '1', 10);
+    if(currentPage !== page) {
+        setPage(currentPage);
     }
 
-    componentDidMount(): void {
-        this.getProducts(this.state.page);
-    }
+    //setPage(currentPage);
+    console.log(products);    
 
-    componentDidUpdate(): void {
-        this.getProducts(this.state.page);
-    }
-
-    render() {
-        return (
-            <Container>
-                <SearchBar />
-                {
-                    this.state.loading ? <Preloader/> :
-                    this.state.products.length ?
-                    ( <Box>
-                        <Products products={this.state.products}/>
-                        <Pagination 
-                            sx={{width: 'fit-content', margin: 'auto'}} 
-                            count={this.state.pagesNumber} 
-                            page={this.state.page} 
-                            onChange={(event: React.ChangeEvent<unknown>,value:number) => {this.setState({page: value})}} />
-                    </Box>
-                     ) : <NothingFound/>
-                }
-                            </Container>
-        );
-    }
+    return (
+        <Container>
+            <SearchBar />
+            {
+                loading ? <Preloader/> :
+                products.length ?
+                ( <Box>
+                    <Products products={products}/>
+                    <Pagination 
+                        sx={{width: 'fit-content', margin: 'auto'}} 
+                        count={pagesNumber} 
+                        page={page}
+                        renderItem={(item) => (
+                            <PaginationItem
+                              component={Link}
+                              to={`${item.page === 1 ? '' : `?page=${item.page}`}`}
+                              {...item}
+                            />
+                          )} 
+                        onChange={(event: React.ChangeEvent<unknown>,value:number) => {setPage(value);}} />
+                </Box>
+                 ) : <NothingFound/>
+            }
+                        </Container>
+    );
 }
 
-export default Main
+export default Main;
